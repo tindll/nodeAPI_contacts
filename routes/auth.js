f88f2fb98ae3240d269738e787e9b780c42c2db4
tindll <zjamsty@gmail.com>
2022-01-17 22:02:26 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -100,8 +100,7 @@ router.get('/contacts', middleware.verify, (req, res) => {
 router.get('/contacts/:id', middleware.verify, (req, res) => {
     try {
         var objID = jwt_decode(req.headers.authorization).data
-        var { id } =req.params
-        //id = mongoose.Types.ObjectId(id)
+        const {id} =req.params
         User.find({"_id": objID, "contacts._id": id})
             .exec(
                 function(err,info) {
