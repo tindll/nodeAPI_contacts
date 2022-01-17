4a3a4de9c7ee93a5993c3a81270c3287e50b64f4
tindll <zjamsty@gmail.com>
2022-01-17 18:16:04 +0100

    updating

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const authR = require('./routes/auth')
 var cors = require("cors");
 
-const  dbURI = "mongodb://work.zjamsty.com/blocksi_app/nodeAPI/testusers12345"
+const  dbURI = "mongodb+srv://dbuser:4KdEHjYvEj9re8Tt@cluster0.8jnyn.mongodb.net/contacts?retryWrites=true&w=majority"
 app.use(cors());
 app.use(express.json())
 app.use('/api/auth',authR)
@@ -13,5 +13,3 @@ mongoose.connect(dbURI , {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on("error", (err)=>{console.error(err)})
 db.once("open", () => {console.log("DB started successfully")})
-
-app.listen (8080, () => console.log("live on http://work.zjamsty.com/blocksi_app/nodeAPI"))
